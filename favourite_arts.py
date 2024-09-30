0dcb2c8920569eaf992ec8bf6dc9c9710d301b7f
Khader23 <168384747+Khader23@users.noreply.github.com>
2024-09-30 12:20:53 +1000

    Update favourite_arts.py

--- favourite_arts.py
+++ favourite_arts.py
@@ -1,15 +1,18 @@
-# favourite_arts.py
-
+ # -*- coding: utf-8 -*-
 import unicodedata
 
-all_emoji = "🐞"
+all_emoji = "🐞" # Example of a non-ASCII character (the emoji)
 columns = ["#", "Emoji", "Name"]
-table_head = f"<thread>\n<tr><th>{'</th><th>' .join(columns)}</th></tr>\n<thread>"
+
+
+print ("About to create table head")
+table_head = f"<thead>\n<tr><th>{'</th><th>'.join(columns)}</th></tr>\n</thead>"
 
 table_body = "\n<tbody>\n"
 for i, emoji in enumerate(all_emoji, start=1):
+    emoji_name = unicodedata.name(emoji)
     emoji_data = [f"{i}.", emoji, unicodedata.name(emoji).title()]
     table_body += f"<tr><td>{'</td><td>'.join(emoji_data)}</td></tr>\n"
-table_body += "</tbody\n>"
-    
+table_body += "</tbody>"
+
 print(f"<table>\n{table_head}{table_body}</table>")
